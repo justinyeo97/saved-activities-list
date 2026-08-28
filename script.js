056512bf73dbc4f5b53bf3cab2a6b3363b628634
Justin Yeo <yeojiaxing97@gmail.com>
2026-08-29 04:51:22 +0800

    getActivity;

--- script.js
+++ script.js
@@ -19,10 +19,32 @@ function userLogin() {
         window.location.href = 'dashboard.html';
     } else {
         alert(`Invalid email or password!`)
-    }
-
-   
+    }  
 }   
 
-document.getElementById('loginButton').addEventListener('click', userLogin);
+async function getAct() {
+    console.log('Get Activity Started')
+  try {
+    const response = await fetch('https://random-activity-sigmo.vercel.app/api/random');
+    const data = await response.json();
+    console.log(data);
+    document.getElementById('activity').innerHTML = `
+      <h2>Activity: ${data.activity}</h2>
+      <p>Type: ${data.type}</p>
+      <p>Participants: ${data.participants}</p>
+      <p>Price: ${data.price}</p>
+      <p>Link: ${data.link}</p>
+      <p>Key: ${data.key}</p>
+      <p>Accessibility: ${data.accessibility}</p>
+      <p>Kid Friendly: ${data.kid_friendly}</p>
+    `;
+  } catch (error) {
+    console.error('Error fetching activity:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('loginButton').addEventListener('click', userLogin);
+  document.getElementById('getAct').addEventListener('click', getAct);
+});
 
