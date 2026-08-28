f88819ea7af2ba7ec8ae9f550f7da51f49479b4b
yeojiaxing97@gmail.com <yeojiaxing97@gmail.com>
2026-08-29 03:51:53 +0800

    Login

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+class User {
+    constructor(email,password) {
+        this.email = email;
+        this.password = password;
+    }
+}
+const users = []
+const admin = new User('yeojiaxing97@mail.com','sigmasigmaboi')
+users.push(admin);
+console.log(users)
+
+function userLogin() {
+    const emailInput = document.getElementById("username").value;
+    const passInput = document.getElementById("password").value;
+    const isValid = users.some(user => user.email === emailInput && user.password === passInput);
+
+    if(isValid) {
+        window.location.href = 'dashboard.html'
+    }
+
+   
+}   
